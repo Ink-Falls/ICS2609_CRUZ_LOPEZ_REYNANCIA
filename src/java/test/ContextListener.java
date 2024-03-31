ec42b55c3ef461cd3ff1e68b424064aa8b013258
HoloDanger <162679391+HoloDanger@users.noreply.github.com>
2024-03-31 23:38:06 +0800

    Made ContextListener public

--- src/java/test/ContextListener.java
+++ src/java/test/ContextListener.java
@@ -5,29 +5,31 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-class ContextListener implements ServletContextListener {
+public class ContextListener implements ServletContextListener {
     private static final String MAIN_HEADER = "C H I P M U N C K S";
     private static final String MAIN_FOOTER = "© 2024";
-    private static final String HIDDEN_FOOTER_MESSAGE = "SOMETIMES IT TAKES A REAL MAN TO BECOME BEST GIRL";
+    private static final String FOOTER_MESSAGE = "SOMETIMES IT TAKES A REAL MAN TO BECOME BEST GIRL";
     private static final String HEADER_ATTRIBUTE = "header";
     private static final String FOOTER_ATTRIBUTE = "footer";
-    private static final String HIDDEN_HEADER_ATTRIBUTE = "hiddenheader";
+    private static final String INITIALIZATION_TIME_ATTRIBUTE = "hiddenheader";
     private static final String HIDDEN_FOOTER_ATTRIBUTE = "hiddenfooter";
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext servletContext = sce.getServletContext();
-        servletContext.setAttribute(HEADER_ATTRIBUTE, MAIN_HEADER);
-        servletContext.setAttribute(FOOTER_ATTRIBUTE, MAIN_FOOTER);
-        servletContext.setAttribute(HIDDEN_HEADER_ATTRIBUTE, LocalDateTime.now().toString());
-        servletContext.setAttribute(HIDDEN_FOOTER_ATTRIBUTE, HIDDEN_FOOTER_MESSAGE);
+        if (servletContext != null) {
+            servletContext.setAttribute(HEADER_ATTRIBUTE, MAIN_HEADER);
+            servletContext.setAttribute(FOOTER_ATTRIBUTE, MAIN_FOOTER);
+            servletContext.setAttribute(INITIALIZATION_TIME_ATTRIBUTE, LocalDateTime.now().toString());
+            servletContext.setAttribute(HIDDEN_FOOTER_ATTRIBUTE, FOOTER_MESSAGE);
+        }
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         ServletContext servletContext = sce.getServletContext();
         if (servletContext != null) {
-            String[] attributeNames = {"header", "footer", "hiddenheader", "hiddenfooter"};
+            String[] attributeNames = {HEADER_ATTRIBUTE, FOOTER_ATTRIBUTE, INITIALIZATION_TIME_ATTRIBUTE, HIDDEN_FOOTER_ATTRIBUTE};
             for (String attributeName : attributeNames) {
                 servletContext.removeAttribute(attributeName);
             }
